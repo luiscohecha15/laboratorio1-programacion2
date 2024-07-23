f4dbd88883703bd255a077bae2047f56fb61b825
luiscohecha15 <luiscohecha15@gmail.com>
2024-07-22 21:02:43 -0500

    p1

--- src/main/java/org/example/lab1/VotationController.java
+++ src/main/java/org/example/lab1/VotationController.java
@@ -4,8 +4,10 @@ import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Label;
 import javafx.stage.Stage;
+
 
 public class VotationController{
     public static int candidato1=0;
@@ -17,6 +19,7 @@ public class VotationController{
     public void BtnVotar1OnAction(ActionEvent event){
 
         candidato1 +=1;
+        
         System.out.println("Candidato 1: "+candidato1);
         //Stage stageActual = (Stage) comeBackBtn.getScene().getWindow();
         //stageActual.close();
@@ -26,6 +29,7 @@ public class VotationController{
     public void BtnVotar2OnAction(ActionEvent event){
 
         candidato2 +=1;
+
         System.out.println("Candidato 2: "+candidato2);
         //Stage stageActual = (Stage) comeBackBtn.getScene().getWindow();
         //stageActual.close();
